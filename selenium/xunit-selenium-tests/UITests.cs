b297e68fb72c97ed78df53e426e9245eb7c7fc29
valle <valle@is-mb.it>
2022-01-19 18:12:02 +0100

    Added Xunit selenium cross browser test

--- selenium/xunit-selenium-tests/UITests.cs
+++ selenium/xunit-selenium-tests/UITests.cs
@@ -6,8 +6,20 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using Xunit.Abstractions;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace xunit_selenium_tests;
+
+public enum BrowserType
+{
+    NotSet,
+    Chrome,
+    Firefox,
+    Edge,
+}
+
 
 public class UITests
 {
@@ -18,8 +30,23 @@ public class UITests
         this.output = output;
     }
 
+    public static IWebDriver Create_Browser(BrowserType browserType)
+    {
+        switch (browserType)
+        {
+            case BrowserType.Chrome:
+                return new ChromeDriver();
+            case BrowserType.Firefox:
+                return new FirefoxDriver();
+            case BrowserType.Edge:
+                return new EdgeDriver();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(browserType), browserType, null);
+        }
+    }
+
     [Fact]
-    public void Test1()
+    public void Test_Chrome_SpecificVersion()
     {
         var chromeOptions = new ChromeOptions();
         // chromeOptions.BrowserVersion = "67";
@@ -32,4 +59,54 @@ public class UITests
         driver.Quit();
 
     }
+
+    [Theory]
+    [InlineData(typeof(ChromeOptions))]
+    [InlineData(typeof(FirefoxOptions))]
+    public void Test_Theory_InlineBrowser(Type DriverOptionsType)
+    {
+        DriverOptions chromeOptions = Activator.CreateInstance(DriverOptionsType) as DriverOptions ?? throw new ArgumentNullException("Invalid type", nameof(DriverOptionsType));
+        IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444"), chromeOptions);
+        driver.Navigate().GoToUrl("http://www.google.com");
+        Console.WriteLine("Prima della wait");
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        output.WriteLine("Dopo la wait");
+        driver.Quit();
+    }
+
+    private static List<object[]> MultiplyByBrowser(List<object[]> test_params)
+    {
+        var retParams = new List<object[]>();
+        foreach (var curr_params in test_params)
+        {
+            // object[] x = new object[] {1, "A"};
+            // x = x.Prepend("1").ToArray();
+            // x = x.Prepend(typeof(ChromeOptions)).ToArray();
+
+            retParams.Add(curr_params.Prepend(typeof(ChromeOptions)).ToArray());
+            retParams.Add(curr_params.Prepend(typeof(FirefoxOptions)).ToArray());
+            retParams.Add(curr_params.Prepend(typeof(EdgeOptions)).ToArray());
+        }
+        return retParams;
+    }
+
+    protected static List<object[]> Test_Parametrized_MultipleBrowsers_Params()
+    {
+        return MultiplyByBrowser(new List<object[]>() {
+            new object[] { "https://www.google.com"},
+            new object[] { "https://www.bing.com"},
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(Test_Parametrized_MultipleBrowsers_Params))]
+    public void Test_Parametrized_MultipleBrowsers(Type DriverOptionsType, string url)
+    {
+        DriverOptions driverOptions = Activator.CreateInstance(DriverOptionsType) as DriverOptions ?? throw new ArgumentNullException("Invalid type", nameof(DriverOptionsType));
+        using (IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444"), driverOptions))
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+
 }
